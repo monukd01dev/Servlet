9693cc1024edc1d28a58c4c3e04fd016d5541aef
monukdpc <monukd01dev@gmail.com>
2023-08-13 19:06:41 +0530

    ServletConfig done

---
+++ IdeaProjects/ServletConfigImplementation/src/main/java/com/servlet/MyServlet.java
@@ -0,0 +1,20 @@
+package com.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        ServletConfig config = getServletConfig();
+        String value = config.getInitParameter("number");
+        out.print("<img src=\"assets/"+value+".jpg\" alt=\"meme\">");
+    }
+}
